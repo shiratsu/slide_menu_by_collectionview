e5baf4c61134b8f326995a48b9f06e611f6c83c6
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2018-05-22 18:30:37 +0900

    途中だけど一旦コミット

---
+++ SampleCollectionMenu/SampleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SampleViewController.swift
+//  SampleCollectionMenu
+//
+//  Created by 平塚 俊輔 on 2018/05/22.
+//  Copyright © 2018年 平塚俊輔. All rights reserved.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
